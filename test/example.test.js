f16175e4c38a9e9fe6fd79d9e4c40edb31ad5f02
chris_chan <chrischandfw@gmail.com>
2022-04-01 15:32:35 -0500

    fixed eslint errors

--- test/example.test.js
+++ test/example.test.js
@@ -5,14 +5,14 @@ const test = QUnit.test;
 
 test('time to test a function for returning number of customers by each purchase frequency', (expect) => {
     const countMap = {
-		Daily: 142
-		Monthly: 123
-		Never: 123
-		Often: 126
-		Once: 107
-		Seldom: 129
-		Weekly: 122
-		Yearly: 128
+		Daily: 142,
+		Monthly: 123,
+		Never: 123,
+		Often: 126,
+		Once: 107,
+		Seldom: 129,
+		Weekly: 122,
+		Yearly: 128,
     };
     const expected = countMap;
     
@@ -25,20 +25,18 @@ test('time to test a function for returning number of customers by each purchase
     expect.deepEqual(actual, expected);
 });
 
-const test = QUnit.test;
-
 test('time to test a function for returning number of customers by cool factor', (expect) => {
     const countMap2 = {
-		1: 109
-		2: 119
-		3: 99
-		4: 101
-		5: 93
-		6: 102
-		7: 101
-		8: 99
-		9: 82
-		10: 95
+		1: 109,
+		2: 119,
+		3: 99,
+		4: 101,
+		5: 93,
+		6: 102,
+		7: 101,
+		8: 99,
+		9: 82,
+		10: 95,
     };
     const expected = countMap2;
     
@@ -51,18 +49,16 @@ test('time to test a function for returning number of customers by cool factor',
     expect.deepEqual(actual, expected);
 });
 
-const test = QUnit.test;
-
 test('time to test a function for returning number of customers by cool factor', (expect) => {
     const countMap3 = {
-		Agender: 17
-		Bigender: 14
-		Female: 457
-		Genderfluid: 18
-		Genderqueer: 11
-		Male: 448
-		Non-binary: 24
-		Polygender: 11
+		Agender: 17,
+		Bigender: 14,
+		Female: 457,
+		Genderfluid: 18,
+		Genderqueer: 11,
+		Male: 448,
+		Non-binary: 24,
+		Polygender: 11,
     };
     const expected = countMap3;
     
